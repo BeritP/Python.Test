e033e32f47d3187c457544b6bf8ae41406377466
BeritP <BeritPorsche@googlemail.com>
2019-05-02 11:35:31 +0200

    neu

--- pi.py
+++ pi.py
@@ -1,2 +1,14 @@
-import wordcloud
+from wordcloud import WordCloud, STOPWORDS 
 import matplotlib.pyplot as plt
+
+with open ("Alice in Wonderland.txt") as f:
+    text = f.read()
+    
+wordcloud = WordCloud(width=1920, height= 1200)
+STOPWORDS.add('said')
+STOPWORDS.add('illustration')
+wordcloud.generate(text)
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis('off')
+plt.show()
+
